34493c318871c61521961078b8446095bdf75d80
Junior Atoche Bravo <junniooralin@gmail.com>
2023-04-13 15:20:27 -0500

    Commit del 13042022

--- src/components/create-chatbox/create-chatbox-component.tsx
+++ src/components/create-chatbox/create-chatbox-component.tsx
@@ -1,129 +1,92 @@
-import {
-  Button,
-  Container,
-  CssBaseline,
-  Grid,
-  Typography,
-} from "@mui/material";
-import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { useThemeContext } from "../../context/theme-context";
-import { useDispatch } from "react-redux";
-import { setAlerts } from "../../reducers";
-import { CustomTextField } from "../partials/custom-material-textfield";
-import { HttpService } from "../../service/http-service";
+import { Button, Container, CssBaseline, Grid, Typography } from "@mui/material"
+import React, { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { useThemeContext } from "../../context/theme-context"
+import { useDispatch } from "react-redux"
+import { createGroup, setAlerts } from "../../reducers"
+import { CustomTextField } from "../partials/custom-material-textfield"
+import { HttpService } from "../../service/http-service"
 
 export const CreateChatBoxComponent = () => {
-  const navigate = useNavigate();
-  const { theme } = useThemeContext();
-  const dispatch = useDispatch();
-  const httpService = new HttpService();
-  const [contactEmail, setContactEmail] = useState("");
-  const [contactDetails, setContactDetails] = useState<any>(null);
+  const navigate = useNavigate()
+  const [groupName, setGroupName] = useState("")
+  const { theme } = useThemeContext()
+  const dispatch = useDispatch()
+  const httpService = new HttpService()
 
   useEffect(() => {
-    document.title = "Create chat box";
-  }, []);
+    document.title = "Create group"
+  }, [])
 
-  async function handleCreateChatBox(event: any) {
-    event.preventDefault();
-    if (!contactDetails) {
-      dispatch(
-        setAlerts({
-          alert: {
-            isOpen: true,
-            alert: "error",
-            text: "Contact not found",
-          },
-        })
-      );
-      return;
-    }
-    const conversationData = {
-      members: [contactDetails._id],
-      isGroup: false,
-    };
-    const response = await httpService.createConversation(conversationData);
-    const conversation = response.data;
-    navigate(`/chatbox/${conversation._id}`);
+  function handleChange (event: any) {
+    event.preventDefault()
+    setGroupName(event.target.value)
   }
 
-  async function handleSearchContact(event: any) {
-    event.preventDefault();
-    if (!contactEmail) {
-      return;
-    }
-    try {
-      const response = await httpService.searchUser(contactEmail);
-      setContactDetails(response.data);
-    } catch (error: any) {
-      setContactDetails(null);
-      const errorMessage =
-        error.response?.data?.message || "Error searching contact";
-      dispatch(
-        setAlerts({
-          alert: {
-            isOpen: true,
-            alert: "error",
-            text: errorMessage,
-          },
-        })
-      );
+  async function createGroupByName (event: any) {
+    event.preventDefault()
+    if (groupName !== "") {
+      const res =  await httpService.createGroup(groupName)
+      dispatch(setAlerts({
+        alert: {
+          isOpen: true,
+          alert: "success",
+          text: `Group "${groupName}" has been created successfully`
+        }
+      }))
+      dispatch(createGroup({ group: res.data }))
+      navigate(`/t/messages/${res.data.url}`)
+      // setAlerts([...alerts, new FeedbackModel(UUIDv4(), `Cannot create group "${groupName}" : ${err.toString()}`, "error", true)])
     }
   }
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setContactEmail(e.target.value);
-  };
-
-  const submitAddContact = (e: React.KeyboardEvent) => {
-    if (e.key === "Enter") {
-      handleSearchContact(e);
+  function submitGroupCreation (event: any) {
+    if (event.key === undefined || event.key === "Enter") {
+      if (groupName === "") {
+        return
+      }
+      createGroupByName(event)
     }
-  };
+  }
 
   return (
-    <div
-      className={theme}
+    <div className={theme}
       style={{
         height: "calc(100% - 64px)",
         textAlign: "center",
-        paddingTop: "40px",
-      }}
-    >
+        paddingTop: "40px"
+      }}>
       <Container className={"clrcstm"} component="main" maxWidth="xs">
-        <CssBaseline />
+        <CssBaseline/>
         <div className={"main-register-form clrcstm"}>
           <Typography className={"clrcstm"} variant="h6">
-            Add Contact to Conversation
+            Create a conversation
           </Typography>
         </div>
         <div className={"clrcstm"}>
           <Grid className={"clrcstm"} container spacing={2}>
             <Grid className={"clrcstm"} item xs={12}>
-              <CustomTextField
-                id={"addContact"}
-                label={"Type contact email"}
-                name={"contactEmail"}
+              <CustomTextField id={"createGroupMessenger"}
+                label={"Type a name for your group"}
+                name={"groupName"}
                 handleChange={handleChange}
-                value={contactEmail}
-                type={"email"}
-                keyUp={submitAddContact}
+                value={groupName}
+                type={"text"}
+                keyUp={submitGroupCreation}
                 isDarkModeEnable={theme}
-                isMultiline={false}
-              />
+                isMultiline={false}/>
             </Grid>
             <div>
               <Grid item xs={12}>
                 <Button
                   className={"button-register-form"}
                   style={{ marginTop: "15px" }}
-                  onClick={handleCreateChatBox}
+                  onClick={(event) => createGroupByName(event)}
                   fullWidth
                   variant="outlined"
                   color="primary"
                 >
-                  Add Contact
+                  Create
                 </Button>
               </Grid>
             </div>
@@ -131,5 +94,5 @@ export const CreateChatBoxComponent = () => {
         </div>
       </Container>
     </div>
-  );
-};
+  )
+}
